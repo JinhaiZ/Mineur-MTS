207b8151ead36df25cfb55050b304d6b0e08d4ba
Jinhai ZHOU <zhoujinhaixd@gmail.com>
2016-04-20 09:34:28 +0200

    finish Question3

--- MTS204/TP1/TP1d.m
+++ MTS204/TP1/TP1d.m
@@ -1,8 +1,10 @@
+clear all;clc;
 %% Question 1
-[Y,FS,NBITS] = wavread('FF_8KHZ.WAV');
-N = length(Y);
-X = linspace(1,N,N)/FS;
-plot(X,Y);
+[X,FS,NBITS] = wavread('FF_8KHZ.WAV');
+N = length(X);
+T = linspace(1,N,N)/FS;
+figure();
+plot(T,X);
 axis([0,N/FS,-1,1])
 xlabel('Temps second');
 ylabel('Amplitude');
@@ -18,17 +20,31 @@ disp(display);
 p = 10;
 nb = T_trame*FS;%nb=160;
 % former la matrice de Toeplitz
+Toeplitz = zeros(p,p,Nbr_trame);
 % calculer les coefficients de predition lineaire
-Toeplitz = zeros(Nbr_trame,p,p);
-a = zeros(Nbr_trame,p);
+a = zeros(p,Nbr_trame);
+% touver la signal derreur de prediction
+e = zeros(nb,Nbr_trame);
 for i = 1:1:Nbr_trame
-    trame = Y(1+160*(i-1):160*i);
-    tau = Tau(trame);
-    temp = tau(1:10);
-    Toeplitz(i,:,:) = toeplitz(temp,temp);
-    A = Toeplitz(i,:,:);
-    a = inv(Toeplitz(i,:,:))*temp;
+    trame = X(1+160*(i-1):160*i);
+    % auto est autocorrelation
+    auto = Autocorrelation(trame);
+    % former la matrice de Toeplitz a partir d'autocorrelation
+    Toeplitz(:,:,i) = toeplitz(auto(1:p));%Gamma(0)~Gamma(p-1)
+    temp = auto(2:p+1);%Gamma(1)~Gamma(p)
+    a(:,i) = -temp/Toeplitz(:,:,i);
+    A = [1;a(:,i)];
+    e(:,i) = filter(A,1,trame);
 end;
+% touver la signal derreur de prediction
+e = reshape(e,1,nb*Nbr_trame);
+figure();
+T = (0:nb*Nbr_trame-1)/FS;
+plot(T, e);
+axis([0,N/FS,-1,1]);
+xlabel('Temps second');
+ylabel('Amplitude');
+title('Signal d''Erreur de prediction');
 
 
 
